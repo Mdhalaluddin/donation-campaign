a1efa8a425cbc96b5d75e5b2231409b9119eba22
Halal1122 <mdhealaluddin505@gmail.com>
2023-09-27 22:26:46 +0600

    Add your commit

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -15,22 +15,22 @@ const myCrateRouter = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: ()=> fetch('../../public/donate.json'),
+                loader: ()=> fetch('donate.json'),
             },
             {
                 path: '/donation',
                 element: <Donation></Donation>,
-                loader: ()=> fetch('../../public/donate.json')
+                loader: ()=> fetch('donate.json')
             },
             {
                 path: '/statistics',
                 element: <Statistics></Statistics>,
-                loader: ()=> fetch('../../public/donate.json')
+                loader: ()=> fetch('donate.json')
             },
             {
                 path: '/cards/:id',
                 element: <DonateCard></DonateCard>,
-                loader: ()=> fetch('../../public/donate.json')
+                loader: ()=> fetch('donate.json')
             }
         ]
     }
